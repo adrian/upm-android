e5f900c72634bbe2ad2cc1d4b4a9efc0ff908354
Adrian Smith <adrian@17od.com>
2010-03-23 20:14:35 +0000

    Only save the database when needs be.
    I also took the opportunity to do the save in onPause() rather than onStop(). The problem with doing it in onStop() is that we were back on the FullAccountList when the save happens. Just looks a a bit weird.

--- src/com/u17od/upm/Prefs.java
+++ src/com/u17od/upm/Prefs.java
@@ -9,20 +9,25 @@ import javax.crypto.IllegalBlockSizeException;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.util.Log;
 
 import com.u17od.upm.database.PasswordDatabase;
 
-public class Prefs extends PreferenceActivity {
+public class Prefs extends PreferenceActivity implements OnPreferenceChangeListener {
 
     private ListPreference sharedURLAuthPref;
     private EditTextPreference sharedURLPref;
     private PasswordDatabase db;
+    private boolean saveRequired;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        saveRequired = false;
 
         // Create the menu items
         addPreferencesFromResource(R.xml.settings);
@@ -30,6 +35,9 @@ public class Prefs extends PreferenceActivity {
         // Get a handle to the preference items
         sharedURLAuthPref = (ListPreference) findPreference("shared_url_auth");
         sharedURLPref = (EditTextPreference) findPreference("shared_url");
+
+        sharedURLAuthPref.setOnPreferenceChangeListener(this);
+        sharedURLPref.setOnPreferenceChangeListener(this);
 
         // Populate the preferences
         db = ((UPMApplication) getApplication()).getPasswordDatabase();
@@ -45,27 +53,44 @@ public class Prefs extends PreferenceActivity {
     }
 
     @Override
-    protected void onStop() {
+    protected void onPause() {
         super.onStop();
 
-        db.getDbOptions().setRemoteLocation(sharedURLPref.getText());
-        db.getDbOptions().setAuthDBEntry(sharedURLAuthPref.getValue());
-        
-        try {
-            db.save();
-        } catch (IllegalBlockSizeException e) {
-            Log.e("Prefs", e.getMessage(), e);
-            String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
-            UIUtilities.showToast(this, message, true);
-        } catch (BadPaddingException e) {
-            Log.e("Prefs", e.getMessage(), e);
-            String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
-            UIUtilities.showToast(this, message, true);
-        } catch (IOException e) {
-            Log.e("Prefs", e.getMessage(), e);
-            String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
-            UIUtilities.showToast(this, message, true);
+        if (saveRequired) {
+            db.getDbOptions().setRemoteLocation(sharedURLPref.getText());
+            db.getDbOptions().setAuthDBEntry(sharedURLAuthPref.getValue());
+            
+            try {
+                db.save();
+            } catch (IllegalBlockSizeException e) {
+                Log.e("Prefs", e.getMessage(), e);
+                String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
+                UIUtilities.showToast(this, message, true);
+            } catch (BadPaddingException e) {
+                Log.e("Prefs", e.getMessage(), e);
+                String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
+                UIUtilities.showToast(this, message, true);
+            } catch (IOException e) {
+                Log.e("Prefs", e.getMessage(), e);
+                String message = String.format(getString(R.string.problem_saving_db), e.getMessage());
+                UIUtilities.showToast(this, message, true);
+            }
         }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == sharedURLAuthPref) {
+            if (!sharedURLAuthPref.getValue().equals(newValue)) {
+                saveRequired = true;
+            }
+        } else if (preference == sharedURLPref) {
+            if (!sharedURLPref.getText().equals(newValue)) {
+                saveRequired = true;
+            }
+        }
+        
+        return saveRequired;
     }
 
 }
